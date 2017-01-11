f14f343ab1c29c9772981181470ea80d7a7cda1e
Sipos Terez <terez.sipos@msg-systems.com>
2017-01-11 16:33:00 +0200

    UserDaoTest

--- bookland_server/src/test/java/edu/msg/bookland/repository/jdbc/UserDaoTest.java
+++ bookland_server/src/test/java/edu/msg/bookland/repository/jdbc/UserDaoTest.java
@@ -7,14 +7,63 @@ import java.util.List;
 import org.junit.Test;
 
 import edu.msg.bookland.model.User;
+import edu.msg.bookland.model.UserType;
+import edu.msg.bookland.repository.RepositoryException;
+import edu.msg.bookland.util.PasswordEncrypting;
 
 public class UserDaoTest {
-	private UserDAO user = new UserDAO();
+	private UserDAO userDao = new UserDAO();
+
+	@Test
+	public void selectAllUserTest() {
+		List<User> users = userDao.getAllUsers();
+		System.out.println("get all users " + users);
+		assertTrue(!users.isEmpty());
+	}
+
+	private User createUser() {
+		User u = new User();
+		u.setName("test34766685");
+		u.setEmail("test378666645@email");
+		u.setLoyaltyIndex(10);
+		u.setPassword(PasswordEncrypting.encrypt("password", "salt"));
+		u.setUserType(UserType.READER);
+		return u;
+	}
+
+	@Test
+	public void insertUserTest() {
+		try {
+			userDao.insertUser(createUser());
+		} catch (RepositoryException e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
+
+	
+	public void updateUserTest() {
+		User u = createUser();
+		u.setName("test12");
+		u.setUUID("37a97280-bb03-4b65-b84d-7602f6b6a86f");
+		try {
+			userDao.updateUser(u);
+		} catch (RepositoryException e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
 	
 	@Test
-	public void selectAllUserTest(){
-		List<User> selectAll = user.getAllUsers();
-		System.out.println("get all users " + selectAll);
-		assertEquals("select * from library_users", selectAll);
+	public void deleteUserTest() {
+		User u = createUser();
+		u.setName("test12");
+		u.setUUID("37a97280-bb03-4b65-b84d-7602f6b6a86f");
+		//try {
+			userDao.deleteUser(u);
+//		} catch (RepositoryException e) {
+//			// TODO: handle exception
+//			System.out.println(e.getMessage());
+//		}
 	}
 }
