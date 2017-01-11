8d8b2674698447400cbfb13f5ca489fe36a48f6f
Jozsef Solomon <Jozsef.Solomon@msg-systems.com>
2017-01-11 11:33:22 +0200

    AuthorDAO interface

---
+++ bookland_server/src/main/java/edu/msg/bookland/repository/AuthorDAO.java
@@ -0,0 +1,15 @@
+package edu.msg.bookland.repository;
+
+import java.util.List;
+
+import edu.msg.bookland.model.Author;
+
+public interface AuthorDAO {
+	List<Author> getAllAuthors();
+
+	Author insertUser(Author author);
+
+	void updateAuthor(Author author);
+
+	void deleteAuthor(Author author);
+}
