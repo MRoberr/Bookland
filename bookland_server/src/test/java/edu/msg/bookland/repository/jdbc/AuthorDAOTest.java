7511b6b50229f355cb1fdb3449ce69b14fdd3307
Jozsef Solomon <Jozsef.Solomon@msg-systems.com>
2017-01-11 13:49:48 +0200

    JDBCAuthorDAO

---
+++ bookland_server/src/test/java/edu/msg/bookland/repository/jdbc/AuthorDAOTest.java
@@ -0,0 +1,22 @@
+package edu.msg.bookland.repository.jdbc;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import edu.msg.bookland.model.Author;
+
+public class AuthorDAOTest {
+	JDBCAuthorDAO authorDAO = new JDBCAuthorDAO();
+	
+	@Test
+	public void getAllAuthors() {
+	List<Author> authors = authorDAO.getAllAuthors();
+	for(Author a:authors) {
+		System.out.println(a);
+	}
+	}
+
+}
