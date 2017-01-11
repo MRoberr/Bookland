8b73d6d19eb85ff26c4209bcffabf2a907be437d
Sipos Terez <terez.sipos@msg-systems.com>
2017-01-11 16:32:06 +0200

    loyaltyIndex

--- bookland_server/src/main/java/edu/msg/bookland/repository/jdbc/UserDAO.java
+++ bookland_server/src/main/java/edu/msg/bookland/repository/jdbc/UserDAO.java
@@ -14,10 +14,7 @@ import edu.msg.bookland.model.User;
 import edu.msg.bookland.model.UserType;
 import edu.msg.bookland.repository.RepositoryException;
 
-public class UserDAO implements edu.msg.bookland.repository.UserDAO{
-	private UserType userType;
-	
-
+public class UserDAO implements edu.msg.bookland.repository.UserDAO {
 	private ConnectionManager conMan;
 	private static final Logger LOGGER = Logger.getLogger(UserDAO.class);
 
@@ -26,7 +23,7 @@ public class UserDAO implements edu.msg.bookland.repository.UserDAO{
 	}
 
 	public List<User> getAllUsers() throws RepositoryException {
-		List<User> list = new ArrayList();
+		List<User> list = new ArrayList<User>();
 		Connection con = null;
 		try {
 			con = conMan.getConnection();
@@ -37,7 +34,7 @@ public class UserDAO implements edu.msg.bookland.repository.UserDAO{
 				u.setName(users.getString("name"));
 				u.setEmail(users.getString("email"));
 				u.setUserType(UserType.valueOf(users.getString("user_type")));
-				u.setLoyaltyIndex(users.getInt("loyality_index"));			
+				u.setLoyaltyIndex(users.getInt("loyalty_index"));
 				u.setUUID(users.getString("uuid"));
 				list.add(u);
 			}
@@ -52,75 +49,83 @@ public class UserDAO implements edu.msg.bookland.repository.UserDAO{
 		}
 		return list;
 	}
-	public User insertUser(User user) throws RepositoryException{
+
+	public void insertUser(User user) throws RepositoryException {
 		Connection con = null;
 		try {
 			con = conMan.getConnection();
 			PreparedStatement preparedStatement = con.prepareStatement("insert into library_users "
-					+ "(name, email, user_type, loiality_index, password) " + "values (?, ?, ?, ?, ?, ?)",
+					+ "(uuid, name, email, user_type, loyalty_index, password) " + "values ( ?, ?, ?, ?, ?, ?)",
 					Statement.RETURN_GENERATED_KEYS);
-			
-			preparedStatement.setString(1, user.getName());
-			preparedStatement.setString(2, user.getEmail());
-			preparedStatement.setString(3, user.getUserType().toString());
-			preparedStatement.setInt(4, user.getLoyaltyIndex());	
-			preparedStatement.setString(5, user.getPassword());
+			preparedStatement.setString(1, user.getUUID());
+			preparedStatement.setString(2, user.getName());
+			preparedStatement.setString(3, user.getEmail());
+			preparedStatement.setString(4, user.getUserType().toString());
+			preparedStatement.setInt(5, user.getLoyaltyIndex());
+			preparedStatement.setString(6, user.getPassword());
 			preparedStatement.execute();
-			ResultSet resultSet = preparedStatement.getGeneratedKeys();
-			resultSet.next();
-			user.setUUID(resultSet.getString(1));
+
 			LOGGER.info("user inserted");
 		} catch (SQLException e) {
-			LOGGER.error("Could not inserd users. ",e);
-			throw new RepositoryException("Could not inser users. ",e);
+			LOGGER.error("Could not inserd users. ", e);
+			throw new RepositoryException("Could not inser users. ", e);
 		} finally {
 			if (con != null) {
 				conMan.returnConnection(con);
 			}
 		}
-		return user;
 	}
-	
+
 	public void deleteUser(User user) throws RepositoryException {
-		// TODO Auto-generated method stub
-		
+		Connection con = null;
+		try {
+			con = conMan.getConnection();
+			PreparedStatement preparedStatement = con.prepareStatement("DELETE FROM library_users WHERE uuid = ?");
+			preparedStatement.setString(1, user.getUUID());
+			preparedStatement.execute();
+
+			LOGGER.info("user deleted");
+			LOGGER.error("user deletted");
+		} catch (SQLException e) {
+			LOGGER.error("Could not delete user. ", e);
+			throw new RepositoryException("Could not delete user. ", e);
+		} finally {
+			if (con != null) {
+				conMan.returnConnection(con);
+			}
+		}
+
 	}
 
 	public void updateUser(User user) throws RepositoryException {
 		Connection con = null;
 		try {
 			con = conMan.getConnection();
-			PreparedStatement preparedStatement = 
-					con.prepareStatement("update library_users set  name=?, email=?, loiality_index=?,  password=? "
-					+"where uuid=?");
-			
-			
+			PreparedStatement preparedStatement = con.prepareStatement(
+					"update library_users set  name=?, email=?, loyalty_index=?,  password=? " + "where uuid=?");
+
 			preparedStatement.setString(1, user.getName());
 			preparedStatement.setString(2, user.getEmail());
 			preparedStatement.setInt(3, user.getLoyaltyIndex());
 			preparedStatement.setString(4, user.getPassword());
-		
+			preparedStatement.setString(5, user.getUUID());
 			preparedStatement.execute();
-			
 			LOGGER.info("user info updated");
+
 		} catch (SQLException e) {
-			LOGGER.error("Could not update user. ",e);
-			throw new RepositoryException("Could not update user. ",e);
+			LOGGER.error("Could not update user. ", e);
+			throw new RepositoryException("Could not update user. ", e);
 		} finally {
 			if (con != null) {
 				conMan.returnConnection(con);
 			}
 		}
-		
+
 	}
 
 	public void updateUserName(User user) throws RepositoryException {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-	
-
-	
 
 }
