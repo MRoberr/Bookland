067bc2433931addfb6ea3eab2ec67820b4ed7c5a
Sipos Terez <terez.sipos@msg-systems.com>
2017-01-11 16:32:21 +0200

    insertUser void

--- bookland_server/src/main/java/edu/msg/bookland/repository/UserDAO.java
+++ bookland_server/src/main/java/edu/msg/bookland/repository/UserDAO.java
@@ -6,7 +6,7 @@ import edu.msg.bookland.model.User;
 
 public interface UserDAO {
 	List<User> getAllUsers()throws RepositoryException;
-	User insertUser(User user) throws RepositoryException;
+	void insertUser(User user) throws RepositoryException;
 	void updateUser(User user) throws RepositoryException;
 	void deleteUser(User user) throws RepositoryException;
 	void updateUserName(User user) throws RepositoryException;
