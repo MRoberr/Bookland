51a2ef964c35cfaf260d167243b1e5c113aadeb3
Jozsef Solomon <Jozsef.Solomon@msg-systems.com>
2017-01-12 15:22:19 +0200

    Szervizek (nincsenek implementalva)

--- bookland_server/src/main/java/edu/msg/bookland/ServerMain.java
+++ bookland_server/src/main/java/edu/msg/bookland/ServerMain.java
@@ -4,7 +4,13 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import edu.msg.bookland.rmi.AuthorServiceRmi;
+import edu.msg.bookland.rmi.BorrowingServiceRmi;
+import edu.msg.bookland.rmi.PublicationServiceRmi;
 import edu.msg.bookland.rmi.UserServiceRmi;
+import edu.msg.bookland.service.AuthorService;
+import edu.msg.bookland.service.BorrowingService;
+import edu.msg.bookland.service.PublicationService;
 import edu.msg.bookland.service.UserService;
 
 public class ServerMain {
@@ -17,9 +23,15 @@ public class ServerMain {
 		try {
 			Registry registry = LocateRegistry.createRegistry(UserServiceRmi.RMI_PORT);
 			
-
 			UserService uService = new UserService();
 			registry.rebind(UserServiceRmi.RMI_NAME, uService);
+			AuthorService aService = new AuthorService();
+			registry.rebind(AuthorServiceRmi.RMI_NAME, aService);
+			BorrowingService boService = new BorrowingService();
+			registry.rebind(BorrowingServiceRmi.RMI_NAME, boService);
+			PublicationService pubService = new PublicationService();
+			registry.rebind(PublicationServiceRmi.RMI_NAME, pubService);
+			
 			System.out.println("Server online!");
 			
 			
