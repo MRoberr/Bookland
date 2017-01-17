608f99df16b089a73d32d2ba0ec80b348494cfdb
Sipos Terez <terez.sipos@msg-systems.com>
2017-01-17 09:14:14 +0200

    Tuple javitasa es javadoc

--- bookland_common/src/main/java/edu/msg/bookland/model/Tuple.java
+++ bookland_common/src/main/java/edu/msg/bookland/model/Tuple.java
@@ -3,6 +3,11 @@ package edu.msg.bookland.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Tuple of a borrowing and a Publication object
+ * @author Terez Sipos
+ *
+ */
 public class Tuple {
 	Borrowing borrow;
 	Publication publication;
@@ -10,14 +15,5 @@ public class Tuple {
 	public Tuple(Borrowing borrow,Publication publication){
 		this.borrow=borrow;
 		this.publication=publication;
-	}
-	
-	public static void main(String...args){
-		Tuple t=new Tuple(new Borrowing(),new Book());
-		List<Tuple> l=new ArrayList<>();
-		l.add(t);
-		for(Tuple tl:l){
-			System.out.println(tl.borrow.getPublicationId());
-		}
 	}
 }
