f13935a10f4bfe8b7b7b08115b8a1b4c2d2e393d
szocsc <szocsc@MSGN04468.int.root.msg.ag>
2017-01-20 16:23:57 +0100

    user crud  muveletek

--- bookland_client/src/main/java/edu/msg/bookland/desktop/controller/DataAdministrationController.java
+++ bookland_client/src/main/java/edu/msg/bookland/desktop/controller/DataAdministrationController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import edu.msg.bookland.common.model.PublicationDTO;
+import edu.msg.bookland.common.model.ServiceException;
 import edu.msg.bookland.common.model.UserDTO;
+import edu.msg.bookland.common.model.UserType;
 import edu.msg.bookland.desktop.model.ConnectionModel;
 
 /**
@@ -44,10 +46,65 @@ public class DataAdministrationController {
 		try {
 			List<UserDTO> list = ConnectionModel.USER_SERVICE_RMI.searchUser(name);
 			return list;
-		} catch (RemoteException e) {
+		} catch (RemoteException e ) {
 			LOGGER.error("No connection when searching publications.", e);
 			return null;
 		}
+	}
+	/**
+	 * This method creates a new user
+	 * @param user
+	 */
+	public void createNewUser(UserDTO user) {
+		
+		try {
+			 ConnectionModel.USER_SERVICE_RMI.insertUser(user);
+		} catch (RemoteException e) {
+			LOGGER.error("No connection when new creating user");
+			e.printStackTrace();
+		}catch (ServiceException s) {
+			LOGGER.error(s.getMessage());
+		}
+	
+	}
+	/**
+	 * This method updates a user 
+	 * @param user
+	 */
+	public void updateUser(UserDTO user){
+		try {
+			ConnectionModel.USER_SERVICE_RMI.updateUser(user);
+		} catch (RemoteException e) {
+			LOGGER.error("No connection when user update");
+			e.printStackTrace();
+		}catch (ServiceException s) {
+			LOGGER.error(s.getMessage());
+		}
+	}
+	/**
+	 * This method deletes a user by user id
+	 * @param userID
+	 */
+	public void deleteUser(String userID){
+		try {
+			ConnectionModel.USER_SERVICE_RMI.deleteUser(userID);;
+		} catch (RemoteException e) {
+			LOGGER.error("No connection when user delete");
+			e.printStackTrace();
+		}catch (ServiceException s) {
+			LOGGER.error(s.getMessage());
+		}
+	}
+	public List<UserDTO>getAllUsers(){
+		try {
+			return ConnectionModel.USER_SERVICE_RMI.getAllUsers();
+		} catch (RemoteException e) {
+			LOGGER.error("no connection when gett all users");
+			e.printStackTrace();
+		}catch (ServiceException s) {
+			LOGGER.error(s.getMessage());
+		}
+		return null;
 	}
 
 }
