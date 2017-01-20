47efff56cedffb8534be2dceb97c902c38135077
Sipos Terez <terez.sipos@msg-systems.com>
2017-01-20 11:52:50 +0200

    PublicationDL done

--- bookland_server/src/main/java/edu/msg/bookland/server/repository/PublicationDAO.java
+++ bookland_server/src/main/java/edu/msg/bookland/server/repository/PublicationDAO.java
@@ -2,27 +2,46 @@ package edu.msg.bookland.server.repository;
 
 import java.util.List;
 
+import edu.msg.bookland.server.business_logic.BusinesLogicException;
 import edu.msg.bookland.server.model.*;
+
 public interface PublicationDAO {
 
-	
 	List<Book> getAllBooks() throws RepositoryException;
+
 	List<Magazine> getAllMagazines() throws RepositoryException;
-	List<Newspaper> getAllNewspapers() throws RepositoryException;	
+
+	List<Newspaper> getAllNewspapers() throws RepositoryException;
+
 	List<Publication> getAllPublications() throws RepositoryException;
-	
+
 	void insertBook(Book book) throws RepositoryException;
+
 	void insertMagazine(Magazine magazine) throws RepositoryException;
+
 	void insertNewspaper(Newspaper newspaper) throws RepositoryException;
-	
+
 	void updateBook(Book book) throws RepositoryException;
+
 	void updateMagazine(Magazine magazine) throws RepositoryException;
+
 	void updateNewspaper(Newspaper newspaper) throws RepositoryException;
-	
+
 	void deletePublication(String uuid) throws RepositoryException;
-	
-	//request methods
-	List<Publication> searchPublication(String title) throws RepositoryException;
+
+	// request methods
+	Publication getPublicationByUuid(String uuid) throws RepositoryException;
+
+	List<Book> searchBook(String title) throws BusinesLogicException;
+
+	List<Magazine> searchMagazine(String title) throws BusinesLogicException;
+
+	List<Newspaper> searchNewspaper(String title) throws BusinesLogicException;
+
+	List<Publication> searchPublication(String title) throws BusinesLogicException;
+
 	int getCopiesLeft(String uuid) throws RepositoryException;
-	void setCopiesLeft(String uuid) throws RepositoryException; //csookent 1el + throw	
+
+	void setCopiesLeft(String uuid) throws RepositoryException; // csookent 1el
+																// + throw
 }
