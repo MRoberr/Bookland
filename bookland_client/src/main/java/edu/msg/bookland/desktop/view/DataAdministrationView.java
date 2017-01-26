c50fd82b4d1f0b8e07929ce7a58f4cea3d3713c8
Simo Zoltan <zoltan.simo@msg-systems.com>
2017-01-26 16:31:28 +0200

    Internationalization fix.

--- bookland_client/src/main/java/edu/msg/bookland/desktop/view/DataAdministrationView.java
+++ bookland_client/src/main/java/edu/msg/bookland/desktop/view/DataAdministrationView.java
@@ -17,7 +17,6 @@ public abstract class DataAdministrationView {
 		System.out.println("1 -" + getProperty("operationUserManagement"));
 		System.out.println("2 -" + getProperty("operationAuthorManagement"));
 		System.out.println("3 -" + getProperty("operationPublicationManagement"));
-		// System.out.println("4 -Borrowing management");
 	}
 
 	public static void menuForAdminDataAUsers() {
@@ -39,28 +38,19 @@ public abstract class DataAdministrationView {
 
 	public static void menuForAdminDataAAuthors() {
 		System.out.println(getProperty("chooseAnOption"));
-		System.out.println("1 -Search for Author");
-		System.out.println("2 -Insert Author");
-		System.out.println("3 -Update Author");
-		System.out.println("4 -Delete Author");
-		// System.out.println("5 -Show all Authors");
-	}
-
-	public static void menuForAdminDataABorrowings() {
-		System.out.println(getProperty("chooseAnOption"));
-		System.out.println("1 -Search for Borrowing");
-		System.out.println("2 -Insert Borrowing");
-		System.out.println("3 -Update Borrowing");
-		System.out.println("4 -Delete Borrowing");
-		// System.out.println("5 -Show all Borrowings");
+		System.out.println("1 -" + getProperty("optionSearchForAuthor"));
+		System.out.println("2 -" + getProperty("optionInsertAuthor"));
+		System.out.println("3 -" + getProperty("optionUpdateAuthor"));
+		System.out.println("4 -" + getProperty("optionDeleteAuthor"));
+		System.out.println("5 -" + getProperty("optionShowAllAuthors"));
 	}
 
 	public static void menuForAdminDataAPublications() {
 		System.out.println(getProperty("chooseAnOption"));
-		System.out.println("1 -Search for Publication");
-		System.out.println("2 -Insert Publication");
-		System.out.println("3 -Update Publication");
-		System.out.println("4 -Delete Publication");
+		System.out.println("1 -" + getProperty("optionSearchForPublication"));
+		System.out.println("2 -" + getProperty("optionInsertPublication"));
+		System.out.println("3 -" + getProperty("optionUpdatePublication"));
+		System.out.println("4 -" + getProperty("optionDeletePublication"));
 		System.out.println("5 -" + getProperty("optionShowAllPublications"));
 	}
 
